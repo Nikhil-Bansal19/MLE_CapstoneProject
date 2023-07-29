224f669cc20300f64df6754729251231047536f8
Nikhil-Bansal19 <47290347+Nikhil-Bansal19@users.noreply.github.com>
2023-07-29 16:44:04 +0000

    all code and files

--- starter_file/train.py
+++ starter_file/train.py
@@ -2,23 +2,23 @@ from sklearn.linear_model import LogisticRegression
 import argparse
 import os
 import numpy as np
+import pandas as pd
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 from azureml.core import Dataset
 
-data_url = 'https://raw.githubusercontent.com/Harini-Pavithra/Machine-Learning-Engineer-with-Microsoft-Azure-Nanodegree/main/Capstone%20Project/Dataset/Heart_Failure_Clinical_Records_Dataset.csv'
+data_url = 'https://raw.githubusercontent.com/Nikhil-Bansal19/MLE_CapstoneProject/master/health%20care%20diabetes.csv'
 ds = TabularDatasetFactory.from_delimited_files(path=data_url)
 #x, y = clean_data(ds)
 # TODO: Split data into train and test sets.
 df = ds.to_pandas_dataframe()
 ### YOUR CODE HERE ###a
-x = df.drop(columns=['DEATH_EVENT'])
-y = df['DEATH_EVENT']
+x = df.drop(columns=['Outcome'])
+y = df['Outcome']
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=42)
 run = Run.get_context()
